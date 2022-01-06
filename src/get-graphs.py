302e0627dd609a87d7b7c2e7822599547848518c
Gabriel Agostini <gsagostini@Gabriels-MacBook-Air.local>
2022-01-06 00:13:57 -0500

    changing graph and preparation bash scripts

--- src/get-graphs.py
+++ src/get-graphs.py
@@ -1,9 +1,9 @@
-import os
 import sys
-from multiprocessing import Pool
 
-from geopandas import read_file
-from osmnx import graph_from_polygon, save_graphml
+import geopandas as gpd
+import osmnx as ox
+
+from datetime import datetime
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -11,30 +11,29 @@ warnings.filterwarnings("ignore")
 ##########################################################################################
 #GOAL: Collect FUA street networks and save them to disk
 
-buffered_shp_filepath = '../data/d03_intermediate/FUA-buffered-shapefile/FUA-buffered.shp'
+fua_code = sys.argv[1]
+start = datetime.now()
 
-# function you want to run in parallel:
-def save_graph(graph_name, boundary, graph_type=sys.argv[1]):
+##########################################################################################
+
+def save_graph(graph_name, boundary, graph_type):
     #Get the graph
-    graph = graph_from_polygon(boundary, network_type=graph_type)
+    graph = ox.graph_from_polygon(boundary, network_type=graph_type)
     #Get the filename
-    filepath = 'graphs/' + graph_type + '/' + graph_name + '.graphml'
+    filepath = '/scratch/g.spessatoagostini/FUA-networks/' + graph_type + '/' + graph_name + '.graphml'
     #Save the graph
-    save_graphml(graph, filepath=filepath)
-    #Document
-    print(graph_name) 
+    ox.save_graphml(graph, filepath=filepath)
     return graph
 
-# list of tuples to serve as arguments to function:
-gdf = read_file(buffered_shp_filepath).set_index('fuacode')
-fua_codes = gdf.index.values
-fua_buffered_boundaries = gdf.geometry.values
-args = list(zip(fua_codes, fua_buffered_boundaries))
+##########################################################################################
 
-# number of cores you have allocated for your slurm task:
-number_of_cores = int(os.environ['SLURM_CPUS_PER_TASK'])
+print('FUA: ', fua_code)
+fua_buffered_boundary = gpd.read_file(buffered_shp_filepath).set_index('fuacode').loc[[fua_code]]
 
-# multiprocssing pool to distribute tasks to:
-with Pool(number_of_cores) as pool:
-    # distribute computations and collect results:
-    results = pool.starmap(save_graph, args)
+drive_graph = save_graph(fua_code, fua_buffered_boundary, 'drive')
+print('  done for drive')
+
+walk_graph = save_graph(fua_code, fua_buffered_boundary, 'walk')
+print('  done for walk')
+
+print('RUNTIME:', datetime.now()-start)
---
+++ src/run-graph.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -l
+#SBATCH -N 1      
+#SBATCH -n 1      
+#SBATCH -c 1
+#SBATCH -t 20:00:00
+#SBATCH --partition=short
+#SBATCH --array=1-162
+
+fua_code=$( awk "NR==$SLURM_ARRAY_TASK_ID" ../data/d02_processed-safegraph/safegraph_fua.txt)
+
+module load anaconda3/3.7
+source activate amenities
+which python
+
+python get-graphs.py $fua_code
