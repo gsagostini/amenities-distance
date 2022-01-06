d0fb3703db2feaa77c187dbd9e20820f07b38a4f
Gabriel Agostini <gsagostini@Gabriels-MacBook-Air.local>
2022-01-05 23:53:53 -0500

    fixing the prepare script

--- src/get-preparedOD.py
+++ src/get-preparedOD.py
@@ -29,11 +29,14 @@ def trim_centroids(od_matrix, buffered_boundary, bdry_as_gdf=True):
 
 print('FUA: ', fua_code)
 
-fua_buffered_boundaries = gpd.read_file('../data/d03_intermediate/FUA-buffered-shapefile/FUA-buffered.shp').set_index('fuacode')
+fua_buffered_boundary = gpd.read_file('../data/d03_intermediate/FUA-buffered-shapefile/FUA-buffered.shp').set_index('fuacode').loc[[fua_code]]
 
-for fua_code, od_matrix in tqdm(od_matrix_dict.items(), total=len(od_matrix_dict)):
-    buffered_boundary = fua_buffered_boundaries.loc[[fua_code]]
-    trimmed_od = trim_centroids(od_matrix, buffered_boundary)
-    trimmed_od.to_csv('/scratch/g.spessatoagostini/d02_processed-safegraph/trimmed-OD-per-FUA/' + fua_code+'_trimmed-ODmatrix.csv')
+full_od_matrix = pd.read_csv('../data/d02_processed-safegraph/weeks_od_us_fua.csv')
+
+od_matrix = full_od_matrix[full_od_matrix.fuacode==fua_code].reset_index(drop=True)
+
+trimmed_od = trim_centroids(od_matrix, fua_buffered_boundary)
+
+trimmed_od.to_csv('/scratch/g.spessatoagostini/d02_processed-safegraph/trimmed-OD-per-FUA/' + fua_code+'_trimmed-ODmatrix.csv')
 
 print('RUNTIME:', datetime.now()-start)
